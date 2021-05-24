970abd7733e676a6c527b70eeb931178ee3bb83b
iskin20003@gmail.com <iskin20003@gmail.com>
2021-05-24 15:04:46 +0300

    First commit in new branch

---
+++ ChessMain/ChessMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChessMain
+{
+    class ChessMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Figure
+    {
+        public int x;
+        public int y;
+
+        public virtual void Move(int x1, int y1)
+        {
+
+        }
+    }
+}
